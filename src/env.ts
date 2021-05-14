02bfdea4b4a3644f5861d829cbb9424decb8fb5e
吴震宇 <wu.zhenyu@darcytech.com>
2021-05-14 09:32:02 +0800

    fix: 删除使用window.name判断是否是localwebview的逻辑.添加如果h5运行环境是手机端,就认为是小程序环境

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
+const isMobile = typeof window !== 'undefined' && /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
+
 // 如果一个窗口没有父窗口,则它的 parent 属性为自身的引用. 即页面不是嵌入在小程序 web-view里的
 // 小程序中没有window 因此需要使用 typeof 判断
-export const isMiniApp = typeof window !== 'undefined' && window.self !== window.top;
+// 加上isMobile的原因是，小程序机器审核是在华为手机上进行的。这里，如果h5页面的环境是手机端，就判断为小程序环境
+export const isMiniApp = (typeof window !== 'undefined' && window.self !== window.top) || isMobile;
 // local web-view 的url 是https://webview/ 开头的
-export const isLocalWebview =
-  typeof window !== 'undefined' && (window.location.href.indexOf('https://webview/') === 0 || window.name === 'localWebView');
+export const isLocalWebview = typeof window !== 'undefined' && window.location.href.indexOf('https://webview/') === 0;
 
 // 页面是嵌入在web-view且不是 local web-view,就是 web-view
 export const isWebView = isMiniApp && !isLocalWebview;
